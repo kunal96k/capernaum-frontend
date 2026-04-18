c16321b9a0842e51d9d0574db4e5968929edece6
unknown <kunalpatil192001@gmail.com>
2026-04-18 18:39:32 +0530

    now check it

---
+++ backend/src/test/java/com/capernaum/bpo/BpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capernaum.bpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
